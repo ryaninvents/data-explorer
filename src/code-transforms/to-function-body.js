7024eaf252016a988ca955cf88f4074a49dfc92d
Ryan Kennedy <r24y@users.noreply.github.com>
2017-10-05 17:58:09 -0400

    Permit correct handling of multiple declarations per Program

--- src/code-transforms/to-function-body.js
+++ src/code-transforms/to-function-body.js
@@ -1,29 +1,41 @@
+import * as t from 'babel-types';
+import generate from 'babel-generator';
+
 /**
  * Transforms statements such as `let z = x + y` into a function body,
  * such as `{ return x + y; }`.
  */
 export default function (babel) {
-    const { types: t } = babel;
   
     return {
       name: "to-function-body",
       visitor: {
-        Program: {
-          exit(path, {opts}) {
-            // Current implementation only allows one declaration per code block.
-            // TODO: generalize
-            opts.onFoundProperties({
-              declares: Object.keys(path.scope.bindings),
-              dependsOn: Object.keys(path.scope.globals),
-            });
-          },
-        },
-        VariableDeclaration(path) {
-          const decl = path.get("declarations.0");
-          const init = decl.get("init");
-          path.replaceWith(
-            t.blockStatement([t.returnStatement(init.node)])
-          );
+        Program(path, {opts}) {
+          const source = path.getSource();
+          const statements = path.get('body');
+          const results = [];
+          statements.forEach(stmt => {
+            if (stmt.isVariableDeclaration) {
+              // TODO: this block assumes only one declarator per declaration; generalize.
+              // e.g. `let x = 5` would work; `let x = 5, y = "foo"` would not
+              const result = {};
+              const decl = stmt.get("declarations.0");
+              const id = decl.get("id");
+              result.declares = id.name;
+              result.depends = Object.keys(decl.scope.globals);
+              const init = decl.get("init");
+              
+              result.functionBody = generate(
+                t.blockStatement([t.returnStatement(init.node)]),
+                {
+                  minified: true,
+                },
+                source
+              );
+              results.push(result);
+            }
+          });
+          opts.onFoundDefinitions(results);
         },
       },
     };
