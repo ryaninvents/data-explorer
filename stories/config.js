7da0217f7359a713973c077f30d423aab92ece26
Ryan Kennedy <me@ryaninvents.com>
2023-01-09 18:57:24 +1000

    chore: delete dead code

--- stories/config.js
+++
@@ -1,8 +0,0 @@
-import { configure } from '@storybook/react';
-
-function loadStories() {
-    // Add `require()` calls here to load all stories.
-    require('./ReplEditor.stories');
-}
-
-configure(loadStories, module);
